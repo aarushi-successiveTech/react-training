0c2b2ba15139bc5c9c96b7e14e0704a19512d3dc
Aarushi Mahajan <aarushi.mahajan@aarushivrs-24.successivetech.com>
2025-06-27 16:50:51 +0530

    completed assignment3

---
+++ feature-assignment3/src/app/page.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 style={{textAlign : 'center'}}>Assignment - 3</h1>
+      <nav style={{textAlign : 'center', fontSize: '30px'}}>
+        <ul>
+          <br/>
+          <li><u><Link href="/question1">Solutions</Link></u></li>
+          
+        </ul>
+      </nav>
+    </div>
+  );
+}
