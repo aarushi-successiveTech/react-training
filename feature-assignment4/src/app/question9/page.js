acd41d0b5dd3a91f9e652d13ae9e71f53753a52b
Aarushi Mahajan <aarushi.mahajan@aarushivrs-24.successivetech.com>
2025-06-30 10:11:14 +0530

    changes

---
+++ feature-assignment4/src/app/question9/page.js
@@ -0,0 +1,2 @@
+/* Implement a responsive sidebar navigation using Material-UI's Drawer component and include it as part of the layout.js so that the sidebar appears consistently across all pages. The sidebar should include links that allow users to navigate between different sections of your web app. */ 
+
